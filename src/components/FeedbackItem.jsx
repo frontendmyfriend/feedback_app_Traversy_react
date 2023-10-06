e434d1fcc2dabd862028f3206457579c5fe14740
Mary Timoshenko <4younguniverse@gmail.com>
2023-10-06 16:54:47 +0300

    added FeedbackStats component and Delete functionality to FeedbackItems

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,51 @@
+import Card from "./shared/Card"
+import PropTypes from 'prop-types'
+import {FaTimes} from 'react-icons/fa'
+
+function FeedbackItem ({item, handleDelete}) {
+
+
+  return (
+    <Card>
+      <div className="num-display">{item.rating}</div>
+
+      <button
+        onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
+
+      <div className="text-display">{item.text}</div>
+
+    </Card>
+  )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired
+}
+
+export default FeedbackItem
+
+// function FeedbackItem ({item}) {
+  
+//   // const [rating, setRating] = useState(7)
+//   // const [text, setText] = useState('This is an example of a Feedback Item yo')
+//   // const handleClick = () => {
+//   //   setRating((prev) => {
+//   //     return prev +1
+//   //   })
+//   // }
+
+//   return (
+//     <div className='card'>
+//          <div className="num-display">{item.rating}</div>
+//       <div className="text-display">{item.text}</div>
+//       {/* <div className="num-display">{rating}</div>
+//       <div className="text-display">{text}</div> */}
+//       {/* <button onClick={handleClick}>Click Me</button> */}
+//       </div>
+//   )
+// }
+
+// export default FeedbackItem
+
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Get familiar with some of the core concepts that make Tailwind CSS different from writing traditional CSS.'
+    },
+    {
+        id: 2,
+        rating: 7,
+        text: 'Run the CLI tool to scan your template files for classes and build your CSS.'
+    },
+    {
+        id: 3,
+        rating: 1,
+        text: 'Add your compiled CSS file to the <head> and start using Tailwind’s utility classes to style your content.'
+    },
+          
+];
+    export default FeedbackData 
