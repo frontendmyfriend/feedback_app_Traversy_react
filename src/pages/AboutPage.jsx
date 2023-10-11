daf213a00205505dc1f4bb53739ed22f2db0f58c
Mary Timoshenko <4younguniverse@gmail.com>
2023-10-11 12:39:35 +0300

    added Routes for new version of react-router-dom

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,14 @@
+import Card from "../components/shared/Card"
+function AboutPage() {
+  return (
+      <Card>
+        <div className="about">
+              <h1>About this project</h1>
+              <p>This is a React app to leave feedback for a product or service</p>
+        </div>
+          
+    </Card>
+  )
+}
+
+export default AboutPage
