0ee0e59277351802c85aa6a8a2585b8ad2ba07ed
Mary Timoshenko <4younguniverse@gmail.com>
2023-10-16 16:09:08 +0300

    added post, aboutLInk, navlink and edit router-dom

--- src/App.js
+++ src/App.js
@@ -1,65 +1,76 @@
-import {v4 as uuidv4}from 'uuid'
-import {useState} from "react"
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Header from "./components/Header"
-import FeedbackList from "./components/FeedbackList"
-import FeedbackData from "./data/FeedbackData"
-import FeedbackStats from "./components/FeedbackStats"
-import FeedbackForm from "./components/FeedbackForm"
-import AboutPage from './pages/AboutPage'
-import AboutIconLink from './components/AboutIconLink'
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, NavLink } from "react-router-dom";
+import Card from "./components/shared/Card";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
+import Post from "./components/Post";
 
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
 
-
-
-function App () {
-    
-    const [feedback, setFeedback] = useState(FeedbackData)
-    
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        if (newFeedback.rating === +'') {
-            alert('Chhose rating')
-        } else {
-            setFeedback([newFeedback, ...feedback])
-        }
-        
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    if (newFeedback.rating === +"") {
+      alert("Chhose rating");
+    } else {
+      setFeedback([newFeedback, ...feedback]);
     }
+  };
 
-    const deleteFeedback = (id) => {
-        if (window.confirm('Are you sure you want to delete?')) {
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
+  };
 
-
-
-    return (
-        <Router>
-            <Header />
-            <div className="container">
-                <Routes>
-                    <Route exact path='/' element={
-                        <>
-                            <FeedbackForm handleAdd={addFeedback}/>
-                            <FeedbackStats feedback={feedback} />
-                            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-                        </>
-                    }>
-                      
-                    </Route>
-                    <Route path='/about' element={<AboutPage />} />
-                </Routes>
+  return (
+    <Router>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
                 <AboutIconLink />
-            </div>
-        </Router>    
-    )
-};
+              </>
+            }
+          ></Route>
+
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/post/:id" element={<Post />} />
+          <Route path="/post/:id/:name" element={<Post />} />
+
+
+        </Routes>
+        <Card>
+            <NavLink className='navlink' to='/' activeClassName='active'>
+              Home  
+            </NavLink >
+            <span></span>
+            <NavLink  className='navlink' to='/about' activeClassName='active'>
+              About  
+            </NavLink>
+        </Card>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
-
-
-
 
 // play with components_________________________________
 // function App () {
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,15 @@
 import Card from "../components/shared/Card"
+import { Link } from "react-router-dom"
+
+
 function AboutPage() {
   return (
       <Card>
         <div className="about">
-              <h1>About this project</h1>
-              <p>This is a React app to leave feedback for a product or service</p>
+          <h1>About this project</h1>
+          <p>This is a React app to leave feedback for a product or service</p>
+          <Link to='/'> Back Home</Link>
         </div>
-          
     </Card>
   )
 }
