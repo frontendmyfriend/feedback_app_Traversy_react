a8fe3ddcd99b6927d1f1e4dd953f15c9777c627a
Mary Timoshenko <4younguniverse@gmail.com>
2023-10-20 15:57:54 +0300

    added Fetch functionality from db.json

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,33 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect} from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children})=>{
+  const [isLoading, setIsLoading]= useState(true)
 
-  const [feedback, setFeedback] = useState([
-    {
-      id:1,
-      text: 'This item is from context 1',
-      rating: 10
-    },
-    {
-      id:2,
-      text: 'This item is from context 2',
-      rating: 7
-    },
-    {
-      id:3,
-      text: 'This item is from context 3',
-      rating: 1
-    },
-
-  ])
+  const [feedback, setFeedback] = useState([])
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false
   })
+
+useEffect(()=>{
+  fetchFeedback()
+}, [])
+
+
+// FETCH FEEDBACK
+const fetchFeedback = async()=>{
+  const response = await fetch ("http://localhost:5000/feedback?_sort=id&_order=desc" );
+  const data = await response.json()
+
+  setFeedback(data)
+  isLoading(false)
+}
 
 
   // add feedback
@@ -63,6 +61,7 @@ export const FeedbackProvider = ({children})=>{
   return <FeedbackContext.Provider value={{
     feedback,
     feedbackEdit,
+    isLoading,
     deleteFeedback,
     addFeedback,
     editFeedback,
